6d573447296262421b4b8b09a91a7e0af24284ac
OCTO619 <ishi2004singh@gmail.com>
2025-07-02 14:22:01 +0530

    Initial commit

---
+++ SportyShoesApp/src/main/java/com/main/SportyShoesApp/SportyShoesAppApplication.java
@@ -0,0 +1,13 @@
+package com.main.SportyShoesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesAppApplication.class, args);
+	}
+
+}
