31542484f611972cea99fcf449aec8193362c759
OCTO619 <ishi2004singh@gmail.com>
2025-07-03 19:59:14 +0530

    business logic added

--- SportyShoesApp/src/main/java/com/service/ProductService.java
+++ SportyShoesApp/src/main/java/com/service/ProductService.java
@@ -3,12 +3,13 @@ package com.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.model.Category;
 import com.model.Product;
 import com.repository.CategoryRepository;
 import com.repository.ProductRepository;
-
+@Service
 public class ProductService {
 	@Autowired
 	private ProductRepository productRepository;
