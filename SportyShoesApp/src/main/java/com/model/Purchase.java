dc36bbae5b4601fb706421e3845f22201b648c2c
OCTO619 <ishi2004singh@gmail.com>
2025-07-03 17:30:50 +0530

    purchase class updated

--- SportyShoesApp/src/main/java/com/model/Purchase.java
+++ SportyShoesApp/src/main/java/com/model/Purchase.java
@@ -1,5 +1,7 @@
 package com.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +16,7 @@ private int id;
 	private User user;
 	@ManyToOne
 	private Product product;
+	private LocalDate purchaseDate;
 	public int getId() {
 		return id;
 	}
